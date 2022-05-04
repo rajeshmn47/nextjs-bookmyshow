17f30e10953574ebe7a16b9f0dbe97517ef61e8b
rajeshmn47 <rajeshmn47@gmail.com>
2022-05-04 15:24:38 +0530

    make it better

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  a=['a','b','c']
+  console.log(a)
+  res.status(200).json({ a})
 }
