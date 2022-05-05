65eee41aa7c9701112ea1965733ea9ccc4fb13fd
rajeshmn47 <rajeshmn47@gmail.com>
2022-05-06 03:31:58 +0530

    make it better

--- pages/api/movie.js
+++ pages/api/movie.js
@@ -13,8 +13,8 @@ export default async function handler(req, res) {
     case 'GET':
       try {
      const movie=Movie({
-         name:'Beast',genre:['Action','Thriller','Suspense'],img_url:'https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzIlICAyMzdrIHZvdGVz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00311733-wsbcgbzuzs-portrait.jpg',
-         description:'Its continuation of KGF1 after rocky kills garuda in the first movie rocky continues doing great things in KGF 2'
+         name:'Acharya',genre:['Action','Thriller','Drama'],img_url:'https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzQlICAyM2sgdm90ZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00126822-yzernpaltd-portrait.jpg',
+         description:'Its movie great mcheeranjeevi and his son acting teacher and student roles'
      }) 
 await movie.save()
         res.status(200).json({ success: true, data:movie })
